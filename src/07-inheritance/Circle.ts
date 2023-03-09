01834be6dad8205ddf840876725d5631b6f58f27
Jeff Campbell <86787426+citizenjett@users.noreply.github.com>
2023-03-08 22:10:11 -0500

    add circle class changes

--- src/07-inheritance/Circle.ts
+++ src/07-inheritance/Circle.ts
@@ -2,13 +2,13 @@ import { Shape } from './Shape';
 
 export class Circle extends Shape {
 
-    
 
-constructor(theX: number, theY: number,
-            private _radius: number
-){
-    super(theX, theY);
-}
+
+    constructor(theX: number, theY: number,
+        private _radius: number
+    ) {
+        super(theX, theY);
+    }
 
     public get radius(): number {
         return this._radius;
@@ -17,8 +17,8 @@ constructor(theX: number, theY: number,
         this._radius = value;
     }
 
-  getInfo(): string{
-    return super.getInfo() + `, radius=${this._radius}`;
-  }
+    getInfo(): string {
+        return super.getInfo() + `, radius=${this._radius}`;
+    }
 
 }
